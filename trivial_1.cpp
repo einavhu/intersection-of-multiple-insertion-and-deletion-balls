2911f46da136b2e2b2e80376e8a467c76272f02a
einavhu <einav.hu@campus.technion.ac.il>
2022-03-03 23:37:32 +0200

    ד

--- trivial_1.cpp
+++ trivial_1.cpp
@@ -1,5 +1,24 @@
-//
-// Created by User on 03/03/2022.
-//
-
+#include <algorithm>
 #include "trivial_1.h"
+
+
+
+vector<set<string>> create_intersection_groups(vector<string> supersequence, vector<string> subsequence){
+   vector<set<string>> S;
+   for(int i=0; i<supersequence.size();i++){
+       CommonSequences cs = CommonSequences(supersequence[i],subsequence[i]);
+       cs.createIntersect();
+       S.push_back(cs.sequence_set);
+   }
+   return S;
+}
+
+
+set<string> algorithm1(vector<string> supersequence, vector<string> subsequence){
+    vector<set<string>> intersection_groups = create_intersection_groups(supersequence,subsequence);
+    std::sort(intersection_groups.begin(), intersection_groups.end(), [](const set<string> & a, const set<string> & b)
+    { return a.size() < b.size();});
+
+}
+
+
--- trivial_1.h
+++ trivial_1.h
@@ -1,5 +1,14 @@
-
 #ifndef INTERSECTION_TRIVIAL_1_H
 #define INTERSECTION_TRIVIAL_1_H
+
+#include <vector>
+#include <string>
+#include <set>
+#include "CommonSequences.hpp"
+
+
+using namespace std;
+
+set<string> algorithm1(vector<string> supersequence, vector<string> subsequence);
 
 #endif //INTERSECTION_TRIVIAL_1_H
