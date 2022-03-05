66310e9b57b42931c987637aa860a83e60c11050
benWarnick <49185038+benWarnick@users.noreply.github.com>
2022-03-05 11:59:18 +0200

    Trivial 2 untested

--- trivial_2.cpp
+++ trivial_2.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "trivial_2.h"
+#include "CommonSequences.hpp"
+
+bool is_subsequence(string sub, string super){
+    int i=0, j=0;
+    while(i < sub.length() && j < super.length()){
+        if (sub[i] == super[j]){
+            i++;
+        }
+        j++;
+    }
+    return i == sub.length();
+}
+
+set<string> algorithm2(vector<string> supersequences, vector<string> subsequences){
+    set<string> result = set<string>();
+    CommonSequences cs = CommonSequences(supersequences[0], subsequences[0]);
+    cs.createIntersect();
+    for (auto i=cs.sequence_set.begin(); i!= cs.sequence_set.end(); ++i){
+        bool flag = true;
+        for(auto j=supersequences.begin(); j!=supersequences.end(); ++j){
+            if (!is_subsequence(*i, *j)){
+                flag = false;
+                break;
+            }
+        }
+        if (!flag){
+            continue;
+        }
+        for(auto j=subsequences.begin(); j!=supersequences.end(); ++j){
+            if (!is_subsequence(*j, *i)){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            result.insert(*i);
+        }
+    }
+    return result;
+}
