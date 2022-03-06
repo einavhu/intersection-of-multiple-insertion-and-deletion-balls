f445845ca2306daa9614ef2bd08750e740e98d6f
Elisheva <elisheva.r@campus.technion.ac.il>
2022-03-06 16:05:09 +0200

    Merge branch 'main' of https://github.com/einavhu/intersection-of-multiple-insertion-and-deletion-balls into main
    # Conflicts:
    #	main.cpp

--- helper.h
+++ helper.h
@@ -1,4 +1,15 @@
 #ifndef INTERSECTION_HELPER_H
 #define INTERSECTION_HELPER_H
 
+#include <string>
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int num_runs(string s);
+string max_runs(set<string>& strings, int* n_p);
+string min_runs(set<string>& strings, int* n_p);
+set<string> best_intersection(set<string>& supers, set<string>& subs);
 #endif //INTERSECTION_HELPER_H
